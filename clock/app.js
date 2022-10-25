57530663ba53e7246e1b54bc1855cd800a2fe434
Daniel Słaby <daniel@dealstack.io>
2022-10-25 19:25:49 +0200

    update

--- clock/app.js
+++ clock/app.js
@@ -1,33 +1,84 @@
-const currentTime = () =>{
+const getCurrentTime = () => {
     const time = new Date();
-    
-    let currentSeconds = time.getSeconds();
-    let currentMinutes = time.getMinutes();
-    let currentHours = time.getHours();
-    
-    const selectedTimeFormat = document.querySelector(`input[name="time_format"]:checked`).value;
-    let session = (selectedTimeFormat === "12h") ? "AM": "";
 
-    if(selectedTimeFormat === "12h"){
-        if(currentHours===0){
-            currentHours=12;
+    const currentSeconds = time.getSeconds();
+    const currentMinutes = time.getMinutes();
+    const currentHours = time.getHours();
+
+    return {
+        currentSeconds,
+        currentMinutes,
+        currentHours,
+    }
+}
+
+const parseTime = (
+    currentSeconds,
+    currentMinutes,
+    currentHours,
+) => {
+    const selectedTimeFormat = document.querySelector(`input[name="time_format"]:checked`).value;
+    let session = (selectedTimeFormat === "12h") ? "AM" : "";
+
+    if (selectedTimeFormat === "12h") {
+        if (currentHours === 0) {
+            currentHours = 12;
         }
-        if(currentHours>12){
-            currentHours = currentHours-12;
+        if (currentHours > 12) {
+            currentHours = currentHours - 12;
             session = "PM";
         }
     }
-    
-    currentHours = (currentHours<10) ? "0"+currentHours:currentHours;
-    currentMinutes = (currentMinutes<10) ? "0"+currentMinutes:currentMinutes;
-    currentSeconds = (currentSeconds<10) ? "0"+currentSeconds:currentSeconds;
+    currentHours = (currentHours < 10) ? "0" + currentHours : currentHours;
+    currentMinutes = (currentMinutes < 10) ? "0" + currentMinutes : currentMinutes;
+    currentSeconds = (currentSeconds < 10) ? "0" + currentSeconds : currentSeconds;
+    return {
+        session,
+        currentSeconds,
+        currentMinutes,
+        currentHours
+    }
+}
 
+const renderTime = (
+    currentSeconds,
+    currentMinutes,
+    currentHours,
+    session
+) => {
     document.querySelector('[data-id="watch"]').textContent = `${currentHours} : ${currentMinutes} : ${currentSeconds} ${session}`;
 }
 
+const updateTime = () => {
+    const {
+        currentSeconds,
+        currentMinutes,
+        currentHours,
+    } = getCurrentTime();
 
-const main =() =>{
-    setInterval(() => {currentTime()}, 1000);
+    const {
+        currentSeconds: parsedCurrentSeconds,
+        currentMinutes: parsedCurrentMinutes,
+        currentHours: parsedCurrentHours,
+        session,
+    } = parseTime(
+        currentSeconds,
+        currentMinutes,
+        currentHours,
+    )
+
+    renderTime(
+        parsedCurrentSeconds,
+        parsedCurrentMinutes,
+        parsedCurrentHours,
+        session
+    )
+
+}
+
+
+const main = () => {
+    setInterval(() => { updateTime() }, 1000);
 }
 
 main();
